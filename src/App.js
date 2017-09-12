971e01989fea0f546d4d007a5400e07640d58c09
Shane <sdarcy@redventures.com>
2017-09-11 22:58:49 -0400

    HTML and CSS ready

--- src/App.js
+++ src/App.js
@@ -9,16 +9,38 @@ class App extends Component {
         <h1>Facebook Messegner App</h1>
         <form>
             <div className="firstMessage">
-                <textarea rows="2" cols="50" autofocus="autofocus" placeholder="What's on your mind?">
+                <textarea rows="3" cols="50" autofocus="autofocus" placeholder="What's on your mind?">
                     
                 </textarea>
                 <input name="Post" type="submit"/>
+                <div className="likes">
+                    <button><img className="thumb" src={require('./thumb_up.png')} /></button>
+                    <p className="like-counter">13</p>
+                    <button><img className="dislike-thumb" src={require('./thumb_up.png')} /></button>
+                    <p className="dislike-counter">6</p>
+                    <button className="comment">comment</button>
+                    <button className="delete">delete</button>
+                </div>
             </div>
-            <div className="replyMessage">
-                <textarea rows="2" cols="40" placeholder="Reply">
-                </textarea>
-                <input name="reply" type="submit" />
-            </div>
+            <ul>
+                <div className="replyMessage">
+                    <div className="commentDiv">
+                        <li>
+                            <textarea rows="3" cols="40" placeholder="Reply">
+                            </textarea>
+                            <input name="reply" type="submit" />
+                        </li>
+                    </div>
+                    <div className="likes">
+                        <button><img className="thumb" src={require('./thumb_up.png')} /></button>
+                        <p className="like-counter">4</p>
+                        <button><img className="dislike-thumb" src={require('./thumb_up.png')} /></button>
+                        <p className="dislike-counter">2</p>
+                        <button className="comment">comment</button>
+                        <button className="delete">delete</button>
+                    </div>
+                </div>
+            </ul>
         </form>
       </div>
     );
